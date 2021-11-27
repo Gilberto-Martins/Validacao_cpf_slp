7bd1e648b9ba9b29bbcffcfdd9940e6d2b8f2a7b
Gilberto Martins <gilbertomartins643@gmail.com>
2021-11-26 23:42:29 -0300

    Validação de 2 digitos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace validação_de_cpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cpf = new string[11];
+            string cpfR;
+
+            Console.WriteLine("Digite seu cpf");
+            cpfR = Console.ReadLine();
+
+            for(int i = 0; i < cpfR.Length; i++)
+            {
+                cpf[i] (cpfR[(cpfR.Length - i - 1)]);
+            }
+
+            Console.WriteLine(cpf);
+            Console.ReadKey();
+        }
+    }
+}
